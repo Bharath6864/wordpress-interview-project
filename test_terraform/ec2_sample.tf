ec7ae8bd72048d73f32ed77535742534c00ce893
BharathK <munisaibharath035877@gmail.com>
2025-05-20 15:01:01 +0530

    commiting the changes with ami id

--- test_terraform/ec2_sample.tf
+++ test_terraform/ec2_sample.tf
@@ -2,11 +2,11 @@ provider "aws" {
     region = "eu-north-1"
 }
 
-resource "aws_instance" "ec2_instance" {
-    ami = "ami-0c02fb55956c7d316"
-    instance_type = "t2.micro"
+resource "aws_instance" "free_tier_ec2" {
+    ami = 	"ami-00f34bf9aeacdf007"
+    instance_type = "t3.micro"
 
-    tag = {
+    tags = {
         name = "test"
     }
     associate_public_ip_address = true
