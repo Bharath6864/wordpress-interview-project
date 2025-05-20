06df89d70fe874873d0bd8071aa2c432345314e3
BharathK <munisaibharath035877@gmail.com>
2025-05-20 16:21:08 +0530

    test passed pushing to main

--- test_terraform/ec2_sample.tf
+++ test_terraform/ec2_sample.tf
@@ -2,10 +2,42 @@ provider "aws" {
     region = "eu-north-1"
 }
 
+data "aws_vpc" "default" {
+    default = "true"
+}
+
+resource "aws_security_group" "allow_ssh_http" {
+    name = "test_vpc"
+    description = " Creating test ec2"
+    vpc_id = data.aws_vpc.default.id
+
+    ingress {
+        from_port = 22
+        to_port = 22
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+    ingress {
+        from_port = 80
+        to_port = 80
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+    egress {
+        from_port = 0
+        to_port = 0
+        protocol = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+    tags = {
+        name = "test"
+    }
+}
+
 resource "aws_instance" "free_tier_ec2" {
     ami = 	"ami-00f34bf9aeacdf007"
     instance_type = "t3.micro"
-
+    vpc_security_group_ids = [aws_security_group.allow_ssh_http.id]
     tags = {
         name = "test"
     }
