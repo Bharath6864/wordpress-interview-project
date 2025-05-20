d1df3e42d210885caa094a1f2b1e273c4ea0d2d4
BharathK <munisaibharath035877@gmail.com>
2025-05-20 11:36:27 +0530

    comminting the region cahanges

---
+++ variables.tf
@@ -0,0 +1,31 @@
+variable "aws_region" {
+  description = "AWS region to deploy resources"
+  default     = "eu-north-1"
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
+  default     = "10.0.0.0/16"
+}
+
+variable "public_subnet_cidrs" {
+  description = "CIDR blocks for public subnets"
+  type        = string
+  default     = ["10.0.1.0/24", "10.0.2.0/24"]
+}
+
+variable "private_subnet_cidrs" {
+  description = "CIDR blocks for private subnets"
+  type        = string
+  default     = ["10.0.3.0/24", "10.0.4.0/24"]
+}
+
+variable "db_username" {
+  description = "Database administrator username"
+  default     = "admin"
+}
+
+variable "db_password" {
+  description = "Database administrator password"
+  sensitive   = true
+}
