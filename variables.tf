166e21f2b735922cbd84f187b9973e652d55b5c0
BharathK <munisaibharath035877@gmail.com>
2025-05-20 11:42:17 +0530

    commiting sudnet

--- variables.tf
+++ variables.tf
@@ -9,14 +9,14 @@ variable "vpc_cidr" {
 }
 
 variable "public_subnet_cidrs" {
-  description = "CIDR blocks for public subnets"
-  type        = string
+  description = "List of CIDR blocks for public subnets"
+  type        = list(string)
   default     = ["10.0.1.0/24", "10.0.2.0/24"]
 }
 
 variable "private_subnet_cidrs" {
-  description = "CIDR blocks for private subnets"
-  type        = string
+  description = "List of CIDR blocks for private subnets"
+  type        = list(string)
   default     = ["10.0.3.0/24", "10.0.4.0/24"]
 }
 
